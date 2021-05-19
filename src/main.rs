5e28afbba1e208fb938d34c831058f1fe400a87d
NrdyBhu1 <NrdyBhu1@gmail.com>
2021-05-19 22:36:38 +0530

    Functionality for problem 1 added: Seems a problem occurs in while and recursion loops

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+fn problem1(arr: Vec<i32>) {
+    println!("Problem 1: ");
+    println!("Write three functions that compute the sum of the numbers in a given list using a for-loop, a while-loop, and recursion.");
+
+    // For loop
+    let mut for_loop_answer: i32 = 0;
+    for i in 0..arr.len() {
+        for_loop_answer += arr[i];
+    }
+    println!("For loop answer: {}", for_loop_answer);
+
+    // while loop
+    let mut while_loop_answer: i32 = 0;
+    let mut loop_over: bool = false;
+    let mut c_index: usize = 0;
+    while !loop_over {
+        if c_index == arr.len() - 1 {
+            loop_over = true;
+        } else {
+            while_loop_answer += arr[c_index];
+            c_index += 1;
+        }
+    }
+    println!("while loop answer: {}", while_loop_answer);
+
+    // recursion
+    let mut recursion_loop_answer: i32 = 0;
+    let mut nc_index: usize = 0;
+    'recursion: loop {
+        if nc_index == arr.len() - 1 {
+            break 'recursion;
+        } else {
+            recursion_loop_answer += arr[nc_index];
+            nc_index += 1;
+        }
+    }
+    println!("recursion loop answer: {}", recursion_loop_answer);
+}
+
 fn main() {
-    println!("Hello, world!");
+    problem1(vec![0, 1, 2, 3, 4, 5, 6]);
 }
